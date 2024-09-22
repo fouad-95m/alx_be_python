98532a358b794485fc5adf6d23e2149a95b707f6
fouad-95m <78822171+fouad-95m@users.noreply.github.com>
2024-09-22 12:30:35 +0100

    Create finance_calculator.py

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,7 @@
+monthly_income = float(input("enrter you monthy income"))
+monthly_expenses = float(input("enrter you monthy monthly_expenses"))
+Savings = float
+monthly_savings = monthly_income - monthly_expenses
+Savings = (monthly_savings * 12 + (monthly_savings * 12 * 0.05))
+print(f'your monthly saving are ${monthly_savings}')
+print(f'your saving is {Savings}')
