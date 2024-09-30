33d2841723d1faaae43ddb9a099fb018e6d93c10
fouad-95m <78822171+fouad-95m@users.noreply.github.com>
2024-09-30 11:39:50 +0100

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,7 +13,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter the item name to add: ")
+            item = input("Enter___ the item name to add: ")
             shopping_list.append(item)
             print(f"{item} has been added to the shopping list.")
         elif choice == '2':
