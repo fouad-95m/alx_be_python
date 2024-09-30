5500cf286575098f66e7e54be8b91d8bca552cfa
fouad-95m <78822171+fouad-95m@users.noreply.github.com>
2024-09-30 11:33:07 +0100

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -13,7 +13,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter the item to add: ")  # Ensuring the exact prompt format
+            item = input("Enter the item to add: \ ")  
             shopping_list.append(item)
             print(f"{item} has been added to the shopping list.")
         elif choice == '2':
