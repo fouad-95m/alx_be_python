c42984f780bcffabaa6c4631eae2ec8e6dfe04dd
fouad-95m <78822171+fouad-95m@users.noreply.github.com>
2024-10-06 20:18:28 +0000

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,7 @@
 def safe_divide(numerator, denominator):
     try:
         numerator = float(numerator)
-        denom = float(denominator)
+        denominator = float(denominator)
          denominator = numerator / denominator
         return f"The result of the division is {result:.2f}"
 
