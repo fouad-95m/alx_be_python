f21e7c52ed1aef3a5bcb713809f325bfd9dfbec4
fouad-95m <78822171+fouad-95m@users.noreply.github.com>
2024-10-06 20:19:49 +0000

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,7 +2,7 @@ def safe_divide(numerator, denominator):
     try:
         numerator = float(numerator)
         denominator = float(denominator)
-         denominator = numerator / denominator
+         result = numerator / denominator
         return f"The result of the division is {result:.2f}"
 
     except ZeroDivisionError:
