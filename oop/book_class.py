f3aad2ea04cd3aaac62196225ad70de0638e5d58
fouad-95m <78822171+fouad-95m@users.noreply.github.com>
2024-10-13 20:27:53 +0100

    Add files via upload

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, title, author, year):
+        """Constructor to initialize a Book instance with title, author, and year."""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """Destructor that prints a message when a Book instance is deleted."""
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """String representation of the Book instance."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Official representation of the Book instance."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
